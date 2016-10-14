2dfd0c990454399a5df02d90118cbdb4bb88920b
Bex Warner <rw1048a@student.american.edu>
2016-10-14 12:57:10 -0400

    deleted old code

--- main.py
+++ main.py
@@ -39,37 +39,11 @@ def rows(expr):
 # This function creates the intial combinations of possible truth values for the variables.
 # Input: the set of variables that are used in the final equation
 # Output: the array of initial truth values
-# def initializeValues(numberOfVariables):
-#     if numberOfVariables < 1:
-#         return [[]]
-#     subtable = initializeValues(numberOfVariables - 1)
-#     return [row + [v] for row in subtable for v in [True, False]]
-
 
 # This function determines the truth value of the ultimate expression based on the inital truth values for that row
 # Input: the array of truth values and the ultimate expression
 # Output: the truth value for a the ultimate expression
 
-# def rows(rowArray):
-#     #returns reults for each individual row given initial columns
-#     #assuming p ^ q
-#     print rowArray
-#     expr = takeInput()
-#     print expr
-#     return expr.subs(rowArray)
-    # OR = False
-    # AND = False
-	#
-    # if AND:
-    #     for x in rowArray:
-    #         if not x:
-    #             return False
-    #     return True
-    # if OR:
-    #     for x in rowArray:
-    #         if x:
-    #             return True
-    #     return False
 
 userInput = raw_input("Write a truth expression: ")
 # userInput = "(A | B) & C & ~B"
