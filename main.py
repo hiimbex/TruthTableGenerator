7c09a7f1e449baec4df61fdf44196f56c2e75dd3
Bex Warner <rw1048a@student.american.edu>
2016-10-14 13:42:44 -0400

    threading with locks working

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import sys, math, threading, pprint
 from sympy import *
 
 class myThread (threading.Thread):
-	def __init__(self, threadID, name, counter):
+	def __init__(self, threadID, name, counter, values, expr, finArr):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.name = name
 		self.counter = counter
+		self.values = values
+		self.expr = expr
+		self.finArr = finArr
 	def run(self):
-		print "Starting " + self.name, self.counter
+		lock = threading.Lock()
+		#print "Starting " + self.name, self.counter
 		# what you want the thread to do during its existence
-		print "Ending " + self.name
+		x = self.values.items()
+		y = self.expr.subs(self.values)
+		lock.acquire()
+		try:
+			self.finArr.append(x)
+			self.finArr.append(y)
+		finally:
+			lock.release()
+		#print "Ending " + self.name, self.finArr
 
 # This is the first function of the program to run.
 # Input: the user's logic expression in the form of a string
@@ -32,16 +44,16 @@ def rows(expr):
     for truth_values in cartes([True, False], repeat=len(variables)):
         # Pair each individual variable with its truth value and add to dictionary
         values = dict(zip(variables, truth_values))
-        thread = myThread(1, "Thread-"+str(counter), counter)
+        thread = myThread(1, "Thread-"+str(counter), counter, values, expr, finalArray)
         thread.start()
         # Append the variable truth values for that row
-        finalArray.append(values.items())
+        #finalArray.append(values.items())
         # Append the ultimate result of the expression for that row in the truth table
-        finalArray.append(expr.subs(values))
+        #finalArray.append(expr.subs(values))
         counter += 1
     return finalArray
 
 
 # userInput = raw_input("Write a truth expression: ")
-userInput = "(A | B) & C & ~B"
+userInput = "a & b"
 print(main(userInput))
