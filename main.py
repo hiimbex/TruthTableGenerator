bfa12eaa07a9cdea94c35f1893ab6832a4814415
Sarah Hendricks <slynnhendricks@gmail.com>
2016-10-21 11:11:30 -0400

    Fixing documentation / style from pylint
    Used pylint styling to adhere to style guide. This includes minimizing
    what we import, class/variable naming conventions, and adding
    documentation in the expected format.

--- main.py
+++ main.py
@@ -1,70 +1,81 @@
-import sys, math, threading, pprint
-from sympy import *
+"""This module creates a logic table from a user-given logic expression."""
+import threading
+from sympy import sympify, cartes
 
-class myThread (threading.Thread):
-    def __init__(self, threadID, name, values, expr, finArr):
+class MyThread(threading.Thread):
+
+    """Creates unique thread.
+
+    This class modifies the existing threading module. Threads in this class
+    are given the variables and a unique combination of their truth values,
+    as well as the given logic expression, to determine the ultimate truth
+    value for that unique combination.
+    """
+
+    def __init__(self, thread_id, values, expression, final_array):
+        """Creates a thread."""
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
+        self.thread_id = thread_id
         self.values = values
-        self.expr = expr
-        self.finArr = finArr
+        self.expression = expression
+        self.final_array = final_array
     def run(self):
+        """Calculates the ultimate truth value from the variables and adds both to the table."""
         # Get the list of variables and their truth values
-        x = self.values.items()
+        variables = self.values.items()
         # Calculate the result of the expression from the variables
-        y = self.expr.subs(self.values)
+        result = self.expression.subs(self.values)
         # Lock the array so we can alter it
-        lock.acquire()
+        LOCK.acquire()
         try:
             # Append the variable truth values for that row
-            self.finArr.append(x)
+            self.final_array.append(variables)
             # Append the ultimate result of the expression for that row in the truth table
-            self.finArr.append(y)
+            self.final_array.append(result)
         finally:
-            lock.release()
+            LOCK.release()
 
-# This function creates the final table of truth values with the variables and the final expression for each row
-# Input: the preformatted logic string
-# Output: the final array
-def rows(expr):
-    finalArray = []
-    threads = []
-    threadID = 1
+def rows(expression):
+    """This function creates the final table of truth values with the variables and the final
+        expression for each row.
+        params: the preformatted logic expression
+        returns: the final array
+    """
+    final_array = []
+    thread_id = 1
     # Get the variables out of the expression
-    variables = expr.free_symbols
+    variables = expression.free_symbols
     # Go through the initial generation of the truth values
-    for truthRow in cartes([True, False], repeat=len(variables)):
+    for truth_row in cartes([True, False], repeat=len(variables)):
         # Pair each individual variable with its truth value and add to dictionary
-        values = dict(zip(variables, truthRow))
+        values = dict(zip(variables, truth_row))
         # Start a unique thread for each row
-        thread = myThread(threadID, "Thread-"+str(threadID), values, expr, finalArray)
+        thread = MyThread(thread_id, values, expression, final_array)
         thread.start()
         # Join the threads together when they finish.
         thread.join()
-        threadID += 1
-    return finalArray
+        thread_id += 1
+    return final_array
 
-# Main part of the program.
 print "This program returns a Truth Table for a given expression."
 print "(and = &, not = ~, or = |, implies = >>)"
 while True:
     print ""
-    userInput = raw_input("Enter 0 to quit. Otherwise enter a valid lowercase truth expression: ").lower()
+    print "Enter 0 to quit."
+    USER_INPUT = raw_input("Enter a valid lowercase truth expression: ").lower()
 
-    if userInput == "0":
+    if USER_INPUT == "0":
         break
     try:
         # Break the input into an expression the computer can understand.
-        expr = sympify(userInput)
+        EXPR = sympify(USER_INPUT)
     except:
-        SyntaxError
-        print "Please enter a valid expression."
-        print "(and = &, not = ~, or = |, implies = >>)"
+        print "NOT A VALID EXPRESSION."
+        print "Accepted symbols: (and = &, not = ~, or = |, implies = >>)"
     else:
-        lock = threading.Lock()
-        table = rows(expr)
+        LOCK = threading.Lock()
+        TABLE = rows(EXPR)
         # Formatting
-        for x in range(0,len(table)):
+        for x in range(0, len(TABLE)):
             if x % 2 == 0:
-                print table[x], table[x+1]
+                print TABLE[x], TABLE[x+1]
